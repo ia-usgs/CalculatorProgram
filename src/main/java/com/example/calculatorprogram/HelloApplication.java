8acb637d35230ff7ffa7b83f39c6aff8b764adda
IrvinAvitia <irvin.i.avitia@gmail.com>
2023-07-08 15:38:38 -0700

    finally fixed the backend stuff

--- src/main/java/com/example/calculatorprogram/HelloApplication.java
+++ src/main/java/com/example/calculatorprogram/HelloApplication.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculator Program");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/calculatorprogram/HelloController.java
+++ src/main/java/com/example/calculatorprogram/HelloController.java
@@ -1,14 +1,81 @@
 package com.example.calculatorprogram;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 public class HelloController {
+
+    // text fields
     @FXML
-    private Label welcomeText;
+    private TextField textField1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textField2;
+
+    @FXML
+    private TextField textField3;
+
+    //buttons
+    @FXML
+    private Label addButton;
+
+    @FXML
+    private Button subtractButton;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Label middleLabel;
+    @FXML
+    protected void addStuff() {
+        float a=Float.parseFloat(textField1.getText());
+        float b=Float.parseFloat((textField2.getText()));
+        float answer=a+b;
+        textField3.setText(Float.toString(answer));
+        middleLabel.setText("Adding your numbers!");
+    }
+    @FXML
+    protected void subtractStuff() {
+        float a=Float.parseFloat(textField1.getText());
+        float b=Float.parseFloat((textField2.getText()));
+        float answer=a-b;
+        textField3.setText(Float.toString(answer));
+        middleLabel.setText("Subtracting your numbers!");
+    }
+    @FXML
+    protected void divideStuff() {
+        float a=Float.parseFloat(textField1.getText());
+        float b=Float.parseFloat((textField2.getText()));
+        float answer=a/b;
+        textField3.setText(Float.toString(answer));
+        middleLabel.setText("Dividing your numbers!");
+    }
+    @FXML
+    protected void multiplyStuff() {
+        float a=Float.parseFloat(textField1.getText());
+        float b=Float.parseFloat((textField2.getText()));
+        float answer=a*b;
+        textField3.setText(Float.toString(answer));
+        middleLabel.setText("Multiplying your numbers!");
+    }
+    @FXML
+    protected void clearStuff() {
+        textField1.setText("");
+        textField2.setText("");
+        textField3.setText("");
+        middleLabel.setText("Clearing your numbers!");
     }
 }
