6139a490d2e73afe9134de0af74e0c0d4f0ec31c
IrvinAvitia <irvin.i.avitia@gmail.com>
2023-07-12 18:39:11 -0700

    looks like I'm done

--- src/main/java/com/example/calculatorprogram/CalculatorApplication.java
+++ src/main/java/com/example/calculatorprogram/CalculatorApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculatorskeleton.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Calculator Program");
         stage.setScene(scene);
--- src/main/java/com/example/calculatorprogram/CalculatorController.java
+++ src/main/java/com/example/calculatorprogram/CalculatorController.java
@@ -6,9 +6,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 import javafx.application.Platform;
-public class HelloController {
 
-    // text fields
+public class CalculatorController {
+
     @FXML
     private TextField textField1;
 
@@ -18,59 +18,51 @@ public class HelloController {
     @FXML
     private TextField textField3;
 
-    //buttons
-    @FXML
-    private Label addButton;
-
-    @FXML
-    private Button subtractButton;
-
-    @FXML
-    private Button multiplyButton;
-
-    @FXML
-    private Button divideButton;
-
-    @FXML
-    private Button exitButton;
-
-    @FXML
-    private Button clearButton;
-
     @FXML
     private Label middleLabel;
+
+    private CalculatorService calculatorService;
+
+    public CalculatorController() {
+        calculatorService = new CalculatorService();
+    }
+
     @FXML
     protected void addStuff() {
-        float a=Float.parseFloat(textField1.getText());
-        float b=Float.parseFloat((textField2.getText()));
-        float answer=a+b;
+        float a = Float.parseFloat(textField1.getText());
+        float b = Float.parseFloat(textField2.getText());
+        float answer = calculatorService.add(a, b);
         textField3.setText(Float.toString(answer));
         middleLabel.setText("Adding your numbers!");
     }
+
     @FXML
     protected void subtractStuff() {
-        float a=Float.parseFloat(textField1.getText());
-        float b=Float.parseFloat((textField2.getText()));
-        float answer=a-b;
+        float a = Float.parseFloat(textField1.getText());
+        float b = Float.parseFloat(textField2.getText());
+        float answer = calculatorService.subtract(a, b);
         textField3.setText(Float.toString(answer));
         middleLabel.setText("Subtracting your numbers!");
     }
+
     @FXML
     protected void divideStuff() {
-        float a=Float.parseFloat(textField1.getText());
-        float b=Float.parseFloat((textField2.getText()));
-        float answer=a/b;
+        float a = Float.parseFloat(textField1.getText());
+        float b = Float.parseFloat(textField2.getText());
+        float answer = calculatorService.divide(a, b);
         textField3.setText(Float.toString(answer));
         middleLabel.setText("Dividing your numbers!");
     }
+
     @FXML
     protected void multiplyStuff() {
-        float a=Float.parseFloat(textField1.getText());
-        float b=Float.parseFloat((textField2.getText()));
-        float answer=a*b;
+        float a = Float.parseFloat(textField1.getText());
+        float b = Float.parseFloat(textField2.getText());
+        float answer = calculatorService.multiply(a, b);
         textField3.setText(Float.toString(answer));
         middleLabel.setText("Multiplying your numbers!");
     }
+
     @FXML
     protected void clearStuff() {
         textField1.setText("");
@@ -78,8 +70,9 @@ public class HelloController {
         textField3.setText("");
         middleLabel.setText("Clearing your numbers!");
     }
+
     @FXML
     protected void exitStuff() {
-            Platform.exit();
+        Platform.exit();
     }
 }
